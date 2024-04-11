27a21e2e1419c847a80012f6f9e4b71361ab9cf9
zl <ng_zhao_loong@tech.gov.sg>
2024-04-11 15:04:07 +0800

    chore: remove fingerprint

--- src/app/custom-event/page.tsx
+++ src/app/custom-event/page.tsx
@@ -1,25 +1,17 @@
 "use client";
 import React from "react";
-import { sendGAEvent } from "@next/third-parties/google";
-import { DataLayer } from "../_components/DataLayer";
-import { randomInt } from "../_utils/randomInt";
 
 export default function CustomEvent() {
-  const [fingerprint, setFingerPrint] = React.useState<number>(0);
   return (
     <>
       <h1>Showcasing custom event:</h1>
       <br />
       <button
         className="data-custom-button"
-        onClick={() => {
-          setFingerPrint(randomInt());
-        }}
       >
         send custom event
       </button>
       <br />
-      <DataLayer fingerprint={fingerprint} />
     </>
   );
 }
